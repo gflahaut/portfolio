a1bea5ebe447dfa5a5cc1f5a0c8833b2061868df
flahaut <flahaut.gwendoline@gmail.com>
2025-12-10 13:14:15 +0100

    error fixing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container px-4 px-lg-5 text-center text-white-50">
-        Copyright &copy; 4woln4tion4rt Website 2024 <FontAwesomeIcon icon={faHeart} />
+        Copyright &copy; 4woln4tion4rt Website 2025 <FontAwesomeIcon icon={faHeart} />
       </div>
     </footer>
   );
